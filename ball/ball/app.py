bea27bf3e8ba540728c9df957e7a09a990730255
Jiroshima <jirodavidiscool@gmail.com>
2023-03-29 12:05:54 +0100

    init

--- ball/ball/app.py
+++ ball/ball/app.py
@@ -1,24 +1,29 @@
+import select
 from flask import Flask, render_template, request
 import requests
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime 
+from flask_sqlalchemy import SQLAlchemy	
+from datetime import datetime
+from sqlalchemy import Identity 
 
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usernames.db'
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
-
-class usernames(db.Model):
+class User(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	username = db.Column(db.String(200), nullable = False)
-	date_created = db.column(db.DateTime, default=datetime.utcnow)
+	username = db.Column(db.String, unique = True, nullable = False)
+	password = db.Column(db.String, nullable = False)
+	date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 	def __repr__(self):
 		return '<name %r>' % self.id
 
-Users = []
 
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route('/')
@@ -29,17 +34,37 @@ def index():
 @app.route("/profile")
 def about():
 	return render_template("profile.html")
+	
+@app.route("/register")
+def register():
+	return render_template("register.html")
 
-@app.route("/login")
-def login():
+@app.route("/api/register", methods=["POST"])
+def add_user():
+	username = request.form.get("username")
+	password = request.form.get("password")
+
+	user = User(username=username, password=password)
+	db.session.add(user)
+	db.session.commit()
+
 	return render_template("login.html")
 
-@app.route("/form", methods=["POST"])
-def form():
-	Username = request.form.get("Username")
-	Password = request.form.get("Password")
-	Users.append(f"{Username} || {Password}")
-	return render_template("form.html", Users=Users)
+@app.route("/login", methods=["POST", "GET"])
+def login():
+	if request.method == "POST":
+		username = request.form.get("username")
+		password = request.form.get("password")
+		user_in_db = db.session.execute(db.select(User).filter_by(username=username)).scalar()
+		print(user_in_db)
+		if user_in_db is None:
+			print("No user found")
+		else:
+			if user_in_db.password == password:
+				return render_template("loggedin.html")
+ 
+	return render_template("login.html")
+
 
 @app.route("/api/games") 
 def getGames():
